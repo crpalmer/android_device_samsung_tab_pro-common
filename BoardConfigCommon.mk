28e7b5add14fceb749b457809c171ed0f20e25a7
Christopher R. Palmer <crpalmer@gmail.com>
2015-01-27 07:17:41 -0500

    fixup with update gps for L
    Change-Id: I37031f9f539e98f878902c6765f4feb1e44ad347

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 
 # GPS
 TARGET_NO_RPC := true
-TARGET_GPS_HAL_PATH := device/samsung/mondrianwifi/gps
+TARGET_GPS_HAL_PATH := device/samsung/tabpro-common/gps
 
 # Graphics
 TARGET_HAVE_NEW_GRALLOC := true
